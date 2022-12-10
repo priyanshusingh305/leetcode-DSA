ff8b152d972ab19752b2350884cdabb287133e25
Priyanshu Singh <72139258+priyanshusingh305@users.noreply.github.com>
2022-12-10 23:04:21 +0530

    Time: 314 ms (37.29%), Space: 78.1 MB (45.83%) - LeetHub

---
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+//
+     int totalsum(TreeNode* root){
+        if(!root) return 0;
+        return totalsum(root->left)+totalsum(root->right)+root->val;
+    }
+    
+    int productmax(TreeNode* root,long int Totalsum,long int &ans){
+        if(!root) return 0;
+        int subtree_sum = 0;  // sum of left child + right child + root
+        subtree_sum += productmax(root->left,Totalsum,ans);
+        subtree_sum += productmax(root->right,Totalsum,ans);
+        subtree_sum += root->val;
+        if(subtree_sum*(Totalsum-subtree_sum) > ans){
+            ans = subtree_sum*(Totalsum - subtree_sum);
+        }
+        return subtree_sum;
+    }
+    
+    long int maxProduct(TreeNode* root) {
+       long Totalsum = totalsum(root);
+       long int ans  = 0;
+       productmax(root,Totalsum,ans);
+        return ans%1000000007;
+    }
+};
