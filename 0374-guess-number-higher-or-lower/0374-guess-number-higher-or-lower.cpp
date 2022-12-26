9983efc8a680e1b7b98a9a63ef3ba5b27d4017a7
Priyanshu Singh <72139258+priyanshusingh305@users.noreply.github.com>
2022-12-26 19:20:40 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (24.73%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
@@ -10,6 +10,7 @@
 class Solution {
 public:
     int guessNumber(int n, long s = 1) {
+        //
         while (s <= n)
         {
             int x = (n + s) / 2;        // here, a conversion long -> int -> long happens
